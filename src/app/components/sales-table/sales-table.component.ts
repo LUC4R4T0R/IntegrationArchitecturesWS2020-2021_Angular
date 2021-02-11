80f53f3fc24659bf3865f0c408f6b07aadf941de
LUC4R4T0R <luc4r4t0r@gmail.com>
2021-02-11 15:40:25 +0100

    added subtotal for sales

--- src/app/components/sales-table/sales-table.component.ts
+++ src/app/components/sales-table/sales-table.component.ts
@@ -1,16 +1,32 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-sales-table',
   templateUrl: './sales-table.component.html',
   styleUrls: ['./sales-table.component.css']
 })
-export class SalesTableComponent implements OnInit {
+export class SalesTableComponent implements OnInit, OnChanges {
   @Input() sales;
+  bonus: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  ngOnChanges(changes){
+    if(changes.sales !== undefined){
+      this.calcBonus();
+    }
+  }
+
+  calcBonus(){
+    for(let sale of this.sales){
+      if(sale.bonus !== undefined && typeof sale.bonus === "number"){
+        this.bonus += sale.bonus;
+      }
+    }
   }
 
 }
