44137cbe50435828212d18f9827b41cb9364b831
LUC4R4T0R <luc4r4t0r@gmail.com>
2021-02-20 22:43:26 +0100

    made visibility of menu buttons adapt to current user group

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from "../../services/auth.service";
 import { MenuEntry } from "../../models/menu-entry";
 import { Router } from "@angular/router";
 import {UserService} from "../../services/user.service";
+import {User} from "../../models/user";
 
 @Component({
   selector: 'app-menu',
@@ -11,19 +12,27 @@ import {UserService} from "../../services/user.service";
 })
 export class MenuComponent implements OnInit {
   entries:MenuEntry[] = [
-    new MenuEntry('ia2020', '../overview'),
-    new MenuEntry('Overview', '../overview'),
-    new MenuEntry('User Management', '../users'),
-    new MenuEntry('Settings', '../settings'),
-    new MenuEntry('Swagger', undefined, 'http://localhost:8080/api/swagger-api'),
+    new MenuEntry('ia2020', './', 0, 0),
+    new MenuEntry('ia2020', './', 1, 1),
+    new MenuEntry('Bonus Sheet', './', 1, 1),
+    new MenuEntry('ia2020', '../overview', 2),
+    new MenuEntry('Overview', '../overview', 2),
+    new MenuEntry('User Management', '../users', 2),
+    new MenuEntry('Settings', '../settings', 2),
+    new MenuEntry('Swagger', undefined, 4, undefined, 'http://localhost:8080/api/swagger-api'),
   ];
 
   constructor(private auth:AuthService, private us:UserService, private router:Router) { }
 
   displayName:string;
+  user: User = new User('', '', undefined);
 
   ngOnInit(): void {
-    this.loadDisplayname();
+    this.us.getUserInfo().subscribe(user =>{
+      this.user = user;
+      this.entries[1].path = '../salesman/'+user.employeeId+'/bonus';
+      this.entries[2].path = '../salesman/'+user.employeeId+'/bonus';
+    });
   }
 
   onLogout(){
@@ -31,12 +40,6 @@ export class MenuComponent implements OnInit {
       this.auth.isLoggedIn().subscribe(() =>{
         this.router.navigate(['']);
       });
-    });
-  }
-
-  loadDisplayname(){
-    this.us.getUserInfo().subscribe(user =>{
-      this.displayName = user.displayname;
     });
   }
 }
--- src/app/models/menu-entry.ts
+++ src/app/models/menu-entry.ts
@@ -2,6 +2,8 @@ export class MenuEntry{
   constructor(
     public title:string,
     public path:string,
+    public mingroup: number = 0,
+    public maxgroup: number = Number.MAX_SAFE_INTEGER,
     public href?:string
   ) {  }
 }
