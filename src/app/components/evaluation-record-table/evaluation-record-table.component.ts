03fc8e4ca64f3e577d2bba8befda9f865e537b5c
LUC4R4T0R <luc4r4t0r@gmail.com>
2021-02-20 19:43:02 +0100

    now using dropdown at evaluation record entry creation

--- src/app/components/evaluation-record-table/evaluation-record-table.component.ts
+++ src/app/components/evaluation-record-table/evaluation-record-table.component.ts
@@ -1,7 +1,8 @@
 import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 import { EvaluationRecordEntry } from "../../models/evaluationRecordEntry";
-import { ModalInput } from "../../models/modalInput";
+import {ModalInput, Option} from "../../models/modalInput";
 import { EvaluationRecordEntryService } from "../../services/evaluation-record-entry.service";
+import {SettingsService} from "../../services/settings.service";
 
 @Component({
   selector: 'app-evaluation-record-table',
@@ -16,16 +17,28 @@ export class EvaluationRecordTableComponent implements OnInit {
   @Input() entries:EvaluationRecordEntry[];
   @Output() update = new EventEmitter();
   displayEntryModal:boolean = false;
+  nameOptions: Option[] = [];
   entryModalInputs:ModalInput[] = [
-    new ModalInput('Name', 'name', 'text'),
+    new ModalInput('Name', 'name', 'select', undefined, undefined, false, this.nameOptions),
     new ModalInput('Target Value', 'target', 'number'),
     new ModalInput('Actual Value', 'actual', 'number')
   ];
 
-  constructor(private eve:EvaluationRecordEntryService) { }
+  constructor(private eve:EvaluationRecordEntryService, private ses: SettingsService) { }
 
   ngOnInit(): void {
-
+    this.ses.getSetting('evaluationRecordEntryNames').subscribe(res => {
+      if(res.status === 200){
+        let first = true;
+        for(let name of res.body.value){
+          this.nameOptions.push(new Option(name));
+          if(first){
+            this.entryModalInputs[0].value = name;
+            first = false;
+          }
+        }
+      }
+    });
   }
 
   changeDisplayEntryModal(event:boolean){
